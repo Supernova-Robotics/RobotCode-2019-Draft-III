98da93860bdaa2a36d10af2f7152813ab79c0ba3
Toby Yang <tianer2820@163.com>
2019-02-13 14:51:27 +0800

    remove some annotations

--- 2019 Draft III/src/main/java/frc/robot/OI.java
+++ 2019 Draft III/src/main/java/frc/robot/OI.java
@@ -1,9 +1,3 @@
-/**
- * OI.java 
- * for creating joystick objects and assigning buttons to different commands.
- * 在此处初始化控制手柄，并将手柄按键对应到机器人任务 (command) 上面。
- */
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
@@ -23,13 +17,11 @@ public class OI {
   public static final XboxController stick_1 = new XboxController(1);
 
   /* create button objects, the last number is the id of the button, starting from 1 */
-  /* 创建按钮对象，最后一个数字对应着 DS 上显示的按钮 ID ，从 1 开始 */
   public static Button stick_0_A = new JoystickButton(stick_0, 1);
   public static Button stick_0_B = new JoystickButton(stick_0, 2);
   public static Button stick_0_X = new JoystickButton(stick_0, 3);
   public static Button stick_0_Y = new JoystickButton(stick_0, 4);
   
-
   public static Button stick_1_A = new JoystickButton(stick_1, 1);
   public static Button stick_1_B = new JoystickButton(stick_1, 2);
   public static Button stick_1_X = new JoystickButton(stick_1, 3);
@@ -39,7 +31,6 @@ public class OI {
 
   public OI() {
     /* binding buttons to commands */
-    /* 将任务 (command) 绑定到按钮 */
     stick_0_A.whileHeld(new IntakeCollect(0.4));
     stick_0_B.whileHeld(new IntakeCollect(-0.8));
     stick_0_X.whenPressed(new IntakeLiftTo(3849));
--- 2019 Draft III/src/main/java/frc/robot/subsystems/Chassis.java
+++ 2019 Draft III/src/main/java/frc/robot/subsystems/Chassis.java
@@ -23,11 +23,9 @@ public class Chassis extends Subsystem {
   private SpeedController motor_roller = new Spark(RobotMap.p_PWM_chassis_roller);
 
   /* using the 'default FRC gyro' */
-  /* 使用的是默认的插在RIO上的陀螺仪 */
   private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
   /* the coefficient for the speed of the chassis motor */
-  /* 电机速度系数，用来控制全局电机最大转速 */
   public double global_y_speed = 0.7;
   public double global_z_speed = 0.4;
   
@@ -35,7 +33,6 @@ public class Chassis extends Subsystem {
     super();
     
     /* correct the direction of motor: forward is positive */
-    /* 修正电机方向，向前为正 */
     motor_left_0.setInverted(false);
     motor_left_1.setInverted(false);
     motor_left_2.setInverted(false);
